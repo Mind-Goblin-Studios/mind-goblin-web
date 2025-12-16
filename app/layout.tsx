46d5cb4003cbf61c87abfb3c9962d41db6448519
Daniel Balluff <kavernbeats@gmail.com>
2025-12-16 13:18:39 -0700

    open graph updates more

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,12 +13,12 @@ export const metadata: Metadata = {
     title: "Mind Goblin",
     description: "Next level indie game development",
     url: "https://mindgoblin.gg",
-    siteName: "Mind Goblin Studios",
+    siteName: "Mind Goblin",
     images: [
       {
-        url: "/og-image.png",
-        width: 600,
-        height: 315,
+        url: "https://mindgoblin.gg/og-image.png",
+        width: 512,
+        height: 512,
         alt: "Mind Goblin",
       },
     ],
@@ -26,10 +26,11 @@ export const metadata: Metadata = {
     type: "website",
   },
   twitter: {
-    card: "summary_large_image",
+    card: "summary",
+    site: "@mindgoblin_gg",
     title: "Mind Goblin",
     description: "Next level indie game development",
-    images: ["/og-image.png"],
+    images: ["https://mindgoblin.gg/og-image.png"],
   },
 };
 
