10b0594688201301dc18a40c82fe5176c2584e9f
Daniel Balluff <kavernbeats@gmail.com>
2025-12-16 13:06:33 -0700

    Open Graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mind Goblin",
   description: "Next level indie game development",
+  metadataBase: new URL("https://mindgoblin.gg"),
+  openGraph: {
+    title: "Mind Goblin",
+    description: "Next level indie game development",
+    url: "https://mindgoblin.gg",
+    siteName: "Mind Goblin Studios",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Mind Goblin Studios",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mind Goblin",
+    description: "Next level indie game development",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
